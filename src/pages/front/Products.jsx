57e3a4e4fd8f8fc2f962b8ad14a720c38157537d
ZoeKang <jujuhwa@gmail.com>
2026-02-03 13:40:44 +0800

    新增產品分頁功能

--- src/pages/front/Products.jsx
+++ src/pages/front/Products.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import * as bootstrap from "bootstrap";
 import MessageToast from "../../components/MessageToast";
 import Loading from "../../components/Loading";
+import Pagination from "../../components/Pagination";
 
 const { VITE_API_PATH, VITE_BASE_URL } = import.meta.env;
 
@@ -12,6 +13,7 @@ function Products() {
   const [showMessages, setShowMessages] = useState({ type: "", msgs: [] });
   const [isShowLoading, setIsShowLoading] = useState(true);
   const msgToastRef = useRef(null);
+  const [pagination, setPagination] = useState({});
 
   const [searchParams, setSearchParams] = useSearchParams();
   const category = searchParams.get("category");
@@ -24,11 +26,21 @@ function Products() {
         { params: searchParams },
       );
       setProducts(response.data.products);
+      setPagination(response.data.pagination);
     } catch (error) {
       console.error(error.response?.data);
     } finally {
       setIsShowLoading(false);
     }
+  };
+
+  const onChangePage = (page = 1) => {
+    setSearchParams(() => {
+      let param = {};
+      if (category) param.category = category;
+      if (page) param.page = page;
+      return param;
+    });
   };
 
   useEffect(() => {
@@ -70,59 +82,68 @@ function Products() {
                 相關資料正在努力建置中，敬請期待!
               </div>
             ) : (
-              <div id="product_container" className="d-flex flex-wrap justify-content-start mx-aoto">
-                {products.map((product) => (
-                  <Link
-                    to={`/product/${product.id}`}
-                    className="m-1 text-decoration-none"
-                  >
-                    <div
-                      className="productlist card m-2"
-                      style={{ width: "18rem", borderColor: "#e2cee2" }}
-                      key={product.id}
+              <>
+                <div
+                  id="product_container"
+                  className="d-flex flex-wrap justify-content-start mx-aoto"
+                >
+                  {products.map((product) => (
+                    <Link
+                      to={`/product/${product.id}`}
+                      className="m-1 text-decoration-none"
                     >
-                      <img
-                        src={product.imageUrl}
-                        className="card-img-to object-fit-cover"
-                        style={{
-                          borderRadius: "0px",
-                          height: "300px",
-                          width: "300px",
-                        }}
-                        alt={product.title}
-                      />
-                      <div className="card-body">
-                        <div className="d-flex justify-content-center">
-                          <h5
-                            className="card-title fw-bold fs-6"
-                            style={{ color: "#785571" }}
-                          >
-                            <span
-                              class="badge me-1 mb-1"
-                              style={{
-                                backgroundColor: "#785571",
-                                borderRadius: "0px",
-                              }}
+                      <div
+                        className="productlist card m-2"
+                        style={{ width: "18rem", borderColor: "#e2cee2" }}
+                        key={product.id}
+                      >
+                        <img
+                          src={product.imageUrl}
+                          className="card-img-to object-fit-cover"
+                          style={{
+                            borderRadius: "0px",
+                            height: "300px",
+                            width: "300px",
+                          }}
+                          alt={product.title}
+                        />
+                        <div className="card-body">
+                          <div className="d-flex justify-content-center">
+                            <h5
+                              className="card-title fw-bold fs-6"
+                              style={{ color: "#785571" }}
                             >
-                              {product.category}
-                            </span>
-                            {product.title}
-                          </h5>
-                        </div>
-
-                        <div className="fst-italic mb-2">
-                          <div className="text-secondary text-decoration-line-through">
-                            原價：{product.origin_price}
+                              <span
+                                class="badge me-1 mb-1"
+                                style={{
+                                  backgroundColor: "#785571",
+                                  borderRadius: "0px",
+                                }}
+                              >
+                                {product.category}
+                              </span>
+                              {product.title}
+                            </h5>
                           </div>
-                          <div style={{ color: "#977b91" }}>
-                            特價：{product.price}
+
+                          <div className="fst-italic mb-2">
+                            <div className="text-secondary text-decoration-line-through">
+                              原價：{product.origin_price}
+                            </div>
+                            <div style={{ color: "#977b91" }}>
+                              特價：{product.price}
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </Link>
-                ))}
-              </div>
+                    </Link>
+                  ))}
+                </div>
+                <Pagination
+                  pagination={pagination}
+                  onChangePage={onChangePage}
+                ></Pagination>
+              </>
             )}
           </div>
         )}
